ff1919d84ad09e37fc018c8291fbd1665366c6f5
Brian McPhillips <B.McPhillips89@gmail.com>
2021-09-29 19:55:09 -0700

    Router added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h3>Rick and Morty</h3>
+  </header>
+);
+
+export default Header;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import ListPage from '../ListPage/ListPage';
+import DetailPage from '../DetailPage/DetailPage';
+import Header from '../Header/Header';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 export default function App() {
-  return <ListPage /> ;
+  return (
+    <>
+      <Router>
+        <Header /> 
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(routerProps) => <ListPage {...routerProps} />}
+          />
+          <Route
+            path="/detail/:id"
+            exact
+            render={(routerProps) => <DetailPage {...routerProps} />}
+          />
+        </Switch>
+      </Router>
+    </>
+  );
 }
