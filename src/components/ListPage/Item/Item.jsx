e3815b6c4909a551c91ebbbd2a97fd9f1ce52132
Brian McPhillips <B.McPhillips89@gmail.com>
2021-09-29 19:31:32 -0700

    Refactor Item component to function

--- src/components/ListPage/Item/Item.jsx
+++ src/components/ListPage/Item/Item.jsx
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Item extends Component {
-  static propTypes = {
-    data: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    location: PropTypes.object.isRequired
+const Item = ({
+  data: {
+    name,
+    image,
+    status,
+    location
   }
-  render() {
-    const {
-      data: {
-        name,
-        image,
-        status,
-        location
-      }
-    } = this.props;
-    return (
-      <div>
-        <img src={image} alt={name} />
-        <div>
-          <h3>{name}</h3>
-          <h4>{location.name}</h4>
-          <p>{status}</p>
-        </div>
-      </div>
-    );
-  }
-}
+}) => (
+  <div>
+    <img src={image} alt={name} />
+    <div>
+      <h3>{name}</h3>
+      <h4>{location.name}</h4>
+      <p>{status}</p>
+    </div>
+  </div>
+);
 
+Item.propTypes = {
+  data: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  status: PropTypes.string,
+  location: PropTypes.object
+};
+
+export default Item;
--- src/services/rick-morty-api.js
+++
@@ -1,7 +0,0 @@
-const URL = process.env.REACT_APP_URL;
-
-export const fetchCharacters = () => {
-  return fetch(`${URL}/api/character`)
-    .then(res => res.json())
-    .then(console.log());
-};
